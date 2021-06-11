f67ce8b65a45c80e58b3e975f9bb42181122b8e0
hulyak <hulyakarakayaa06@gmail.com>
2021-06-11 12:11:06 -0700

    feat: create the markup and css

---
+++ app.js
@@ -0,0 +1,17 @@
+let profileEditButton = document.querySelector(".profile__edit-button");
+profileEditButton.addEventListener("click", () => {});
+
+  function handleFormSubmit(evt) {
+    evt.preventDefault();
+    let nameInput = // Use querySelector()
+    let jobInput = // Use querySelector()
+        // Get the values of each field from the corresponding value property
+
+        // Select elements where the field values will be entered
+
+        // Insert new values using the textContent property of the querySelector() method
+  }
+
+// Connect the handler to the form:
+// it will watch the submit event
+formElement.addEventListener('submit', handleFormSubmit);
