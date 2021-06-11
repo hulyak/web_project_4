2b99b0d6cb56f663c39a184f4baf52eafde41d87
hulyak <hulyakarakayaa06@gmail.com>
2021-06-11 16:18:05 -0700

    feat: code elements and replace svg with img tags

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-let profileEditButton = document.querySelector(".profile__edit-button");
-profileEditButton.addEventListener("click", () => {});
+let openFormButton = document.querySelector(".button_type_edit");
+let closeButton = document.querySelector(".popup__close-button");
+let form = document.querySelector(".popup__form");
+let inputName = document.querySelector(".popup__input_type_name");
+let inputJob = document.querySelector(".popup__input_type_job");
+let profileName = document.querySelector(".profile__name");
+let profileJob = document.querySelector(".profile__job");
+let popup = document.querySelector(".popup");
 
-  function handleFormSubmit(evt) {
-    evt.preventDefault();
-    let nameInput = // Use querySelector()
-    let jobInput = // Use querySelector()
-        // Get the values of each field from the corresponding value property
+function toggleForm() {
+  popup.classList.toggle("popup__form_visible");
+}
 
-        // Select elements where the field values will be entered
+openFormButton.addEventListener("click", toggleForm);
+closeButton.addEventListener("click", toggleForm);
 
-        // Insert new values using the textContent property of the querySelector() method
-  }
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  let nameInput = document.querySelector(".popup__input_type_name");
+  let jobInput = document.querySelector(".popup__input_type_job");
+  // Get the values of each field from the corresponding value property
+  // Select elements where the field values will be entered
+  // Insert new values using the textContent property of the querySelector() method
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+}
 
-// Connect the handler to the form:
-// it will watch the submit event
-formElement.addEventListener('submit', handleFormSubmit);
+// Connect the handler to the form: it will watch the submit event
+form.addEventListener("submit", handleFormSubmit);
