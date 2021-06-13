218a73e6d9e21e4e09c6e9e01fac2a9963a12974
hulyak <hulyakarakayaa06@gmail.com>
2021-06-12 18:14:12 -0700

    update styles

--- app.js
+++ app.js
@@ -3,38 +3,42 @@ const closeButton = document.querySelector(".popup__close-button");
 
 const popupContainer = document.querySelector(".popup__container");
 const popup = document.querySelector(".popup");
+const overlay = document.querySelector(".overlay");
 
 // profile properties
 const profileName = document.querySelector(".profile__name");
 const profileOccupation = document.querySelector(".profile__occupation");
 
 const profileForm = document.forms.profile;
+const profileFormNameInput = profileForm.elements.name;
+const profileFormOccupationInput = profileForm.elements.occupation;
 
 openFormButton.addEventListener("click", function () {
+  profileFormNameInput.value = profileName.textContent;
+  profileFormOccupationInput.value = profileOccupation.textContent;
   popupContainer.style.display = "block";
   popup.style.display = "block";
+  overlay.style.opacity = "0.5";
+  // popupContainer.classList.add("popup_opened");
 });
 
-closeButton.addEventListener("click", function () {
+function closePopup() {
   popupContainer.style.display = "none";
   popup.style.display = "none";
-});
+}
+
+closeButton.addEventListener("click", closePopup);
 
 // profileFormNameInput.addEventListener("input", function (event) {
 //   const inputValue = event.target.value;
 //   profileFormNameInput.textContent = inputValue;
 // });
 
-function handleFormSubmit(evt) {
-  evt.preventDefault();
-  const profileFormNameInput = profileForm.elements.name;
-  const profileFormOccupationInput = profileForm.elements.occupation;
-
-  // Get the values of each field from the corresponding value property
-  // Select elements where the field values will be entered
-  // Insert new values using the textContent property of the querySelector() method
-  profileFormNameInput.value = profileName.textContent;
-  profileFormOccupationInput.value = profileOccupation.textContent;
+function handleFormSubmit(event) {
+  event.preventDefault();
+  profileName.textContent = profileFormNameInput.value;
+  profileOccupation.textContent = profileFormOccupationInput.value;
+  closePopup();
 }
 
 // Connect the handler to the form: it will watch the submit event
