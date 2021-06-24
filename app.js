4388b83283cdb19f6c240ca92908902fbcbdd7fa
hulyak <hulyakarakayaa06@gmail.com>
2021-06-24 16:11:42 -0700

    generate the image gallery with template

--- app.js
+++ app.js
@@ -3,6 +3,51 @@ const closeButton = document.querySelector(".popup__close-button");
 
 const popup = document.querySelector(".popup");
 
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+  },
+];
+
+function createCard(data) {
+  const elements = document.querySelector("#elements").content;
+  const element = elements.querySelector(".element").cloneNode(true);
+  element.querySelector(".element__image").src = data.link;
+  element.querySelector(".element__text").textContent = data.name;
+  element
+    .querySelector(".element__like-button")
+    .addEventListener("click", function (evt) {
+      evt.target.classList.toggle("element__like-button_active");
+    });
+  return element;
+}
+
+const elementsList = document.querySelector(".elements__list");
+initialCards.forEach((card) => {
+  elementsList.prepend(createCard(card));
+});
+
 // profile properties
 const profileName = document.querySelector(".profile__name");
 const profileOccupation = document.querySelector(".profile__occupation");
