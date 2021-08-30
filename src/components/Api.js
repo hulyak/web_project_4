b659f9d4fc8fbcae0add91ef5dfb4fcbd9688288
hulyak <hulyakarakayaa06@gmail.com>
2021-08-29 17:46:33 -0700

    add new card

--- src/components/Api.js
+++ src/components/Api.js
@@ -33,6 +33,17 @@ class Api {
       return Promise.reject(`Error: ${res.status}`);
     });
   }
+
+  addNewCard() {
+    return fetch(`${this._baseUrl}/cards`, {
+      headers: this._headers,
+      method: "POST",
+      body: JSON.stringify({ name: this.name, link: this.link }),
+    }).then((res) => {
+      if (res.ok) return res.json();
+      return Promise.reject(`Error: ${res.status}`);
+    });
+  }
 }
 
 export default Api;
